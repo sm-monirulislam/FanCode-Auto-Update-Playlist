41f55d805ee9b791ab4d2f2f51ab7d2e7393af9f
Monirul islam Shakib <monirulislamshakib1718@gmail.com>
2026-02-09 10:48:23 +0600

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -7,29 +7,19 @@ EXTRACT_FOLDER = "extracted"
 def unzip_main():
     password = os.getenv("ZIP_PASSWORD")
     if not password:
-        raise ValueError("❌ ZIP_PASSWORD not found!")
+        raise ValueError("ZIP_PASSWORD not found")
 
-    password_bytes = password.encode("utf-8") 
-    print("🔐 Extracting ZIP...") 
-    
-    if not os.path.exists(EXTRACT_FOLDER): 
-        os.makedirs(EXTRACT_FOLDER) 
-        
-    with pyzipper.AESZipFile(ZIP_FILE) as zf: 
-        zf.pwd = password_bytes 
-        zf.extractall(EXTRACT_FOLDER) 
-        print("📂 Extracted to:", EXTRACT_FOLDER) 
-        
-    # Copy extracted files to root 
-    for f in os.listdir(EXTRACT_FOLDER): 
-        src = os.path.join(EXTRACT_FOLDER, f) 
-        dst = os.path.join(".", f) 
-        
-        if os.path.isfile(src): 
-            with open(src, "rb") as s, open(dst, "wb") as d: 
-                d.write(s.read()) 
-                
-    print("🎉 Unzip Completed — Files moved to root!") 
+    pwd = password.encode("utf-8")
+
+    print("🔐 Extracting ZIP...")
+
+    os.makedirs(EXTRACT_FOLDER, exist_ok=True)
+
+    with pyzipper.AESZipFile(ZIP_FILE) as zf:
+        zf.pwd = pwd
+        zf.extractall(EXTRACT_FOLDER)
+
+    print("✅ Extracted inside:", EXTRACT_FOLDER)
 
 if __name__ == "__main__":
     unzip_main()
